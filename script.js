23a57c357bd363fa07b64add2d73913fdde7a018
MariaeuzEbio8 <150376181+MariaeuzEbio8@users.noreply.github.com>
2026-06-08 10:26:08 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+function mostrarMensagem() {
+    document.getElementById("mensagem").innerHTML =
+        "🌱 O trabalhador rural merece respeito, reconhecimento e valorização por sua contribuição para a sociedade!";
+}
